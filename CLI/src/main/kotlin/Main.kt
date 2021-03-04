427f48117aba15bfb972bc33ce775b4b7c8a382c
ligi <ligi@ligi.de>
2021-03-04 16:58:29 +0100

    Add CLI module for DX

---
+++ CLI/src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import org.devcon.dipchecker.checkFolder
+import java.io.File
+
+fun main() {
+    println(checkFolder(File("/home/ligi/git/devcon/DIPs/DIPs")))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 include("DIP-checker")
+include("CLI")
