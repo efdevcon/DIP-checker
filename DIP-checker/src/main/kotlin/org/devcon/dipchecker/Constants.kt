d173bb5afa13a83cb4bdcb5e42505d06dfc2eadb
ligi <ligi@ligi.de>
2023-05-04 14:54:07 +0200

    Add instances header field

--- DIP-checker/src/main/kotlin/org/devcon/dipchecker/Constants.kt
+++ DIP-checker/src/main/kotlin/org/devcon/dipchecker/Constants.kt
@@ -5,5 +5,5 @@ import java.text.SimpleDateFormat
 val imageExtensions = listOf("svg", "jpg", "jpeg", "png")
 
 val mandatoryHeaders = listOf("DIP", "Title", "Status", "Themes", "Tags", "Authors", "Discussion", "Created")
-val optionalHeaders = listOf("Resources Required", "Requires")
+val optionalHeaders = listOf("Resources Required", "Requires", "Instances")
 val allHeaders = mandatoryHeaders + optionalHeaders
